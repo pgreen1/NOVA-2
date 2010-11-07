872146773e393f5c13c9fb633e11971f36571d6e
Phillip Green II <pgreen1@mix.wvu.edu>
2010-11-07 01:39:32 -0400

    reorganized mymodel and added scoring functions

---
+++ mymodel-engine/src/main/clojure/edu/wvu/lcsee/green/mymodel/scoringfunctions.clj
@@ -0,0 +1,46 @@
+(ns edu.wvu.lcsee.green.mymodel.scoringfunctions
+	(:import (edu.wvu.lcsee.green.mymodel.model MyModelAttribute MyModelAttribute$ProjectSize)
+             (java.lang Math))
+	;(:require )
+)
+
+(gen-class
+  :name edu.wvu.lcsee.green.mymodel.CostScoringFunction
+  :implements [edu.wvu.lcsee.green.spi.ScoringFunction]
+  :prefix "cost-")
+
+(defn cost-getKey [this]
+  "cost")
+
+(defn cost-score [this project]
+  (*  (Math/pow 2 (.getValueFor project MyModelAttribute/FF))
+      (condp =  (.getValueFor project MyModelAttribute/PSIZE)
+          MyModelAttribute$ProjectSize/SMALL  1.0
+          MyModelAttribute$ProjectSize/MEDIUM 3.0
+          MyModelAttribute$ProjectSize/LARGE  9.0)
+      (/ (.getValueFor project MyModelAttribute/MEXP)
+          10)))
+
+(gen-class
+  :name edu.wvu.lcsee.green.mymodel.DurationScoringFunction
+  :implements [edu.wvu.lcsee.green.spi.ScoringFunction]
+  :prefix "duration-")
+
+(defn duration-getKey [this]
+  "duration")
+
+(defn duration-score [this project]
+   (/ (* (Math/pow 15  (.getValueFor project MyModelAttribute/FF))
+        (condp =  (.getValueFor project MyModelAttribute/PSIZE)
+          MyModelAttribute$ProjectSize/SMALL  2.0
+          MyModelAttribute$ProjectSize/MEDIUM 1.0
+          MyModelAttribute$ProjectSize/LARGE  0.5))
+      (.getValueFor project MyModelAttribute/MEXP)))
+
+;
+;(let [scenerio (.generateScenario edu.wvu.lcsee.green.mymodel.model.MyModelConfigurationDefinition/MY_MODEL_CONFIGURATION
+;                  edu.wvu.lcsee.green.mymodel.model.MyModelConfigurationDefinition/ALL
+;                  edu.wvu.lcsee.green.mymodel.model.MyModelConfigurationDefinition/DEFAULT)
+;      projectGenerator (.getProjectGenerator edu.wvu.lcsee.green.ui.NovaControl/INSTANCE)
+;      project (.generate projectGenerator scenerio)]
+;  (cost-score nil project))
--- mymodel/src/main/java/edu/wvu/lcsee/green/mymodel/Application.java
+++ mymodel/src/main/java/edu/wvu/lcsee/green/mymodel/Application.java
@@ -1,10 +1,14 @@
-
 package edu.wvu.lcsee.green.mymodel;
 
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+import edu.wvu.lcsee.green.model.Project;
 import edu.wvu.lcsee.green.model.Scenario;
 import edu.wvu.lcsee.green.mymodel.model.MyModelConfigurationDefinition;
 import edu.wvu.lcsee.green.spi.ProjectGenerator;
+import edu.wvu.lcsee.green.spi.ScoringFunction;
 import edu.wvu.lcsee.green.ui.NovaControl;
+import java.util.Map;
 
 /**
  *
@@ -15,9 +19,19 @@ public class Application {
   public static void main(final String[] args) {
     final ProjectGenerator projectGenerator = NovaControl.INSTANCE.getProjectGenerator();
 
-    final Scenario scenario = MyModelConfigurationDefinition.MY_MODEL_CONFIGURATION.generateScenario(MyModelConfigurationDefinition.ALL, MyModelConfigurationDefinition.DEFAULT);
+    final Scenario scenario = MyModelConfigurationDefinition.MY_MODEL_CONFIGURATION.generateScenario(
+            MyModelConfigurationDefinition.ALL, MyModelConfigurationDefinition.DEFAULT);
 
     System.out.println(projectGenerator.generateMany(scenario, 20));
-  }
 
+    final Project project = projectGenerator.generate(scenario);
+
+    final Map<String, Number> scores = Maps.newHashMap();
+    for (final Map.Entry<String, ScoringFunction> entry : NovaControl.INSTANCE.getScoringFunctions().entrySet()) {
+      scores.put(entry.getKey(), entry.getValue().score(project));
+    }
+    System.out.println(ImmutableMap.copyOf(scores));
+
+
+  }
 }
