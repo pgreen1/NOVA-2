8c324288e704109a306c45b55349de08d21ef3ca
Phillip Green II <pgreen1@mix.wvu.edu>
2010-11-09 21:56:12 -0500

    Split Path into Path and PathImpl

---
+++ nova-api/src/main/java/edu/wvu/lcsee/green/search/impl/PathImpl.java
@@ -0,0 +1,72 @@
+package edu.wvu.lcsee.green.search.impl;
+
+import java.util.List;
+import javax.annotation.Nonnull;
+import edu.wvu.lcsee.green.search.*;
+import com.google.common.collect.ImmutableList;
+import java.util.Date;
+import edu.wvu.lcsee.green.search.Path;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ *
+ * @author pdgreen
+ */
+public class PathImpl implements Path {
+
+  private final Date timeStarted;
+  private final Date timeEnded;
+  private final ImmutableList<State> states;
+
+  public PathImpl(@Nonnull final Date timeStarted, @Nonnull final Date timeEnded, @Nonnull final List<State> states) {
+    this.timeStarted = checkNotNull(timeStarted);
+    this.timeEnded = checkNotNull(timeEnded);
+    this.states = ImmutableList.copyOf(states);
+  }
+
+  @Override
+  public ImmutableList<State> getStates() {
+    return states;
+  }
+
+  @Override
+  public Date getTimeEnded() {
+    return timeEnded;
+  }
+
+  @Override
+  public Date getTimeStarted() {
+    return timeStarted;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    final PathImpl other = (PathImpl) obj;
+    if (this.timeStarted != other.timeStarted && (this.timeStarted == null || !this.timeStarted.equals(other.timeStarted))) {
+      return false;
+    }
+    if (this.timeEnded != other.timeEnded && (this.timeEnded == null || !this.timeEnded.equals(other.timeEnded))) {
+      return false;
+    }
+    if (this.states != other.states && (this.states == null || !this.states.equals(other.states))) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 5;
+    hash = 61 * hash + (this.timeStarted != null ? this.timeStarted.hashCode() : 0);
+    hash = 61 * hash + (this.timeEnded != null ? this.timeEnded.hashCode() : 0);
+    hash = 61 * hash + (this.states != null ? this.states.hashCode() : 0);
+    return hash;
+  }
+  //TODO overRide toString
+}
