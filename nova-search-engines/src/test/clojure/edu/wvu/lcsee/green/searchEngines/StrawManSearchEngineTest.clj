74c5d10c330e732afa4957d1224c995f55cb5d02
Phillip Green II <pgreen1@mix.wvu.edu>
2011-01-02 00:58:54 -0500

    created unit tests for StrawmanSearchEngine

---
+++ nova-search-engines/src/test/clojure/edu/wvu/lcsee/green/searchEngines/StrawManSearchEngineTest.clj
@@ -0,0 +1,18 @@
+(ns edu.wvu.lcsee.green.searchEngines.StrawManSearchEngineTest
+  ;(:require)
+  (:use clojure.test edu.wvu.lcsee.green.searchEngines.mockobjects)
+  ;(:import)
+  )
+
+
+(deftest StrawManSearchEngine-key-should-be-strawman
+  (let [searchEngine (new edu.wvu.lcsee.green.searchEngines.StrawManSearchEngine)]
+    (is (= "strawman" (.getKey searchEngine)))))
+
+
+(deftest StrawManSearchEngine-should-search
+  (let [searchEngine (new edu.wvu.lcsee.green.searchEngines.StrawManSearchEngine)
+        evaluationFunction (generate-mock-evaluationFunction (fn [_] 10))
+        initialScenario (generate-mock-scenario)
+        result (.search searchEngine evaluationFunction initialScenario)]
+    (is result)))
