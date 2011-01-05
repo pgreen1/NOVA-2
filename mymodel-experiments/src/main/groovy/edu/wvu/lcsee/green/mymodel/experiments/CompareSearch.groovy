6c84a676103b99be32c23e00ec10941619ecb8dc
Phillip Green II <pgreen1@mix.wvu.edu>
2011-01-05 11:19:49 -0500

    updated CompareSearch to use new ModelConfiguration

--- mymodel-experiments/src/main/groovy/edu/wvu/lcsee/green/mymodel/experiments/CompareSearch.groovy
+++ mymodel-experiments/src/main/groovy/edu/wvu/lcsee/green/mymodel/experiments/CompareSearch.groovy
@@ -1,23 +1,15 @@
-
 package edu.wvu.lcsee.green.mymodel.experiments
 
-import edu.wvu.lcsee.green.mymodel.CostScoringFunction;
 import edu.wvu.lcsee.green.search.Path;
 import edu.wvu.lcsee.green.search.SearchEngine;
 import edu.wvu.lcsee.green.search.EvaluationFunction;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.collect.Maps;
 import edu.wvu.lcsee.green.control.NovaBootstrap;
 import edu.wvu.lcsee.green.control.NovaControl;
 import edu.wvu.lcsee.green.model.Project;
 import edu.wvu.lcsee.green.model.Scenario;
 import edu.wvu.lcsee.green.model.ProjectGenerator;
-import edu.wvu.lcsee.green.model.ScoredProject;
-import edu.wvu.lcsee.green.model.ScoringFunction;
-import java.util.Date;
-import java.util.Map;
-import static edu.wvu.lcsee.green.mymodel.model.MyModelConfigurationDefinition.*;
+import edu.wvu.lcsee.green.mymodel.model.MyModelConfiguration;
+
 
 import edu.wvu.lcsee.green.search.impl.MeanProjectScoreEvaluationFunction;
 
@@ -25,9 +17,11 @@ final NovaControl novaControl = NovaBootstrap.loadFactory().newInstance();
 
 final ProjectGenerator projectGenerator = novaControl.getProjectGenerator();
 
-final Scenario scenario = MY_MODEL_CONFIGURATION.generateScenario(ATTRIBUTE_CONTEXT_DEFAULT, CASE_STUDY_DEFAULT);
+final MyModelConfiguration myModelConfiguration = novaControl.getModelConfigration(MyModelConfiguration.class);
 
-final EvaluationFunction evaluationFunction = new MeanProjectScoreEvaluationFunction(projectGenerator,    100, new CostScoringFunction());
+final Scenario scenario = myModelConfiguration.generateDefaultScenario();
+
+final EvaluationFunction evaluationFunction = new MeanProjectScoreEvaluationFunction(projectGenerator, 100, myModelConfiguration.getCostScoringFunction());
 
 
 def runSearchEngine(searchEngine, evaluationFunction, scenario) {
