c04670d1effac991471f8a1ff24476ed2f1301c2
Phillip Green II <pgreen1@mix.wvu.edu>
2011-01-05 11:16:33 -0500

    extracted StandardAttribute out of MyModelAttribute into DefaultAttribute

--- mymodel-model/src/main/java/edu/wvu/lcsee/green/mymodel/model/MyModelAttribute.java
+++ mymodel-model/src/main/java/edu/wvu/lcsee/green/mymodel/model/MyModelAttribute.java
@@ -1,18 +1,18 @@
 package edu.wvu.lcsee.green.mymodel.model;
 
+import edu.wvu.lcsee.green.model.impl.DefaultAttribute;
 import edu.wvu.lcsee.green.model.Attribute;
-import java.io.Serializable;
-import javax.annotation.Nonnull;
 
 /**
- *
+ * Attributes used in MyModel.
+ * 
  * @author pdgreen
  */
-public interface MyModelAttribute {
+public final class MyModelAttribute {
 
-  public final static Attribute<Double> FF = new StandardAttribute<Double>("FF", "Fudge Factor", Double.class);
-  public final static Attribute<Integer> MEXP = new StandardAttribute<Integer>("MEXP", "Mean Experience", Integer.class);
-  public final static Attribute<ProjectSize> PSIZE = new StandardAttribute<ProjectSize>("PSIZE", "Project Size",
+  public final static Attribute<Double> FF = new DefaultAttribute<Double>("FF", "Fudge Factor", Double.class);
+  public final static Attribute<Integer> MEXP = new DefaultAttribute<Integer>("MEXP", "Mean Experience", Integer.class);
+  public final static Attribute<ProjectSize> PSIZE = new DefaultAttribute<ProjectSize>("PSIZE", "Project Size",
           ProjectSize.class);
 
   public enum ProjectSize {
@@ -20,39 +20,7 @@ public interface MyModelAttribute {
     SMALL, MEDIUM, LARGE;
   }
 
-  static class StandardAttribute<V extends Serializable> implements Attribute {
-
-    private final String name;
-    private final String description;
-    private final Class<V> valueType;
-
-    public StandardAttribute(final @Nonnull String name, final @Nonnull String description,
-            final @Nonnull Class<V> valueType) {
-      this.name = name;
-      this.description = description;
-      this.valueType = valueType;
-    }
-
-    @Override
-    public String getName() {
-      return name;
-    }
-
-    @Override
-    public String getDescription() {
-      return description;
-    }
-
-    @Override
-    public Class<V> getValueType() {
-      return valueType;
-    }
-
-    @Override
-    public String toString() {
-      return getName();
-    }
-
-
+  private MyModelAttribute() {
+    super();
   }
 }
---
+++ nova-api/src/main/java/edu/wvu/lcsee/green/model/impl/DefaultAttribute.java
@@ -0,0 +1,43 @@
+package edu.wvu.lcsee.green.model.impl;
+
+import edu.wvu.lcsee.green.model.Attribute;
+import java.io.Serializable;
+import javax.annotation.Nonnull;
+
+public class DefaultAttribute<V extends Serializable> implements Attribute {
+
+  private final String name;
+  private final String description;
+  private final Class<V> valueType;
+
+  public DefaultAttribute(@Nonnull
+  final String name,
+          @Nonnull
+  final String description,
+          @Nonnull
+  final Class<V> valueType) {
+    this.name = name;
+    this.description = description;
+    this.valueType = valueType;
+  }
+
+  @Override
+  public String getName() {
+    return name;
+  }
+
+  @Override
+  public String getDescription() {
+    return description;
+  }
+
+  @Override
+  public Class<V> getValueType() {
+    return valueType;
+  }
+
+  @Override
+  public String toString() {
+    return getName();
+  }
+}
