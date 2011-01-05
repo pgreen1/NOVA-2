2eb8ddc78c69ed764ed070d2d02146980755c6d2
Phillip Green II <pgreen1@mix.wvu.edu>
2011-01-05 11:18:14 -0500

    added ModelConfigurationForwaringImpl so that custom ModelConfiguration implementations can be created, but still use the ModelConfigurationBuilder

---
+++ nova-api/src/main/java/edu/wvu/lcsee/green/model/impl/ModelConfigurationForwardingImpl.java
@@ -0,0 +1,63 @@
+package edu.wvu.lcsee.green.model.impl;
+
+import com.google.common.collect.ImmutableSet;
+import edu.wvu.lcsee.green.model.Attribute;
+import edu.wvu.lcsee.green.model.CaseStudy;
+import edu.wvu.lcsee.green.model.Constraints;
+import edu.wvu.lcsee.green.model.ModelConfiguration;
+import edu.wvu.lcsee.green.model.NamedConstrainableAttributes;
+import edu.wvu.lcsee.green.model.Scenario;
+import edu.wvu.lcsee.green.model.ScoringFunction;
+import java.io.Serializable;
+
+/**
+ * Implementation for {@link ModelConfiguration} where all methods delegate to the 
+ * ModelConfiguration returned by {@link ModelConfigurationForwardingImpl#delegate()}.
+ *
+ * @author pdgreen
+ */
+public abstract class ModelConfigurationForwardingImpl implements ModelConfiguration {
+
+  /**
+   * This method should return the delegate for forwarding of methods.
+   *
+   * @return the delegate to forward methods onto.
+   */
+  protected abstract ModelConfiguration delegate();
+
+  @Override
+  public ImmutableSet<Attribute<? extends Serializable>> getAllAttributes() {
+    return delegate().getAllAttributes();
+  }
+
+  @Override
+  public <V extends Serializable> Constraints<V> getDefaultConstraintsFor(Attribute<V> attribute) {
+    return delegate().getDefaultConstraintsFor(attribute);
+  }
+
+  @Override
+  public NamedConstrainableAttributes getDefaultConstrainableAttributes() {
+    return delegate().getDefaultConstrainableAttributes();
+  }
+
+  @Override
+  public Scenario generateScenario(final NamedConstrainableAttributes namedConstrainableAttributes,
+          final CaseStudy caseStudy) {
+    return delegate().generateScenario(namedConstrainableAttributes, caseStudy);
+  }
+
+  @Override
+  public Scenario generateDefaultScenario() {
+    return delegate().generateDefaultScenario();
+  }
+
+  @Override
+  public ImmutableSet<ScoringFunction> getAllScoringFunctions() {
+    return delegate().getAllScoringFunctions();
+  }
+
+  @Override
+  public ScoringFunction getScoringFunctionForKey(final String key) {
+    return delegate().getScoringFunctionForKey(key);
+  }
+}
