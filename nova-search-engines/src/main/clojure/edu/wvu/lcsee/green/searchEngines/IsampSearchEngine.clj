4d474699618a3ff470f7aaa2ce349883def795ed
Phillip Green II <pgreen1@mix.wvu.edu>
2011-01-06 03:43:02 -0500

    restructred IsampSearchEngine for two reasons: fix a bug where it always returned the initial state and pull out constrain-scenario-fn

--- nova-search-engines/src/main/clojure/edu/wvu/lcsee/green/searchEngines/IsampSearchEngine.clj
+++ nova-search-engines/src/main/clojure/edu/wvu/lcsee/green/searchEngines/IsampSearchEngine.clj
@@ -15,7 +15,7 @@
             (not (.isFullyConstrained (.getConstraintsFor scenario attribute))))
     (.getConstrainableAttributes scenario)))
 
-(defn constrainScenario [scenario attributesLeftToConstrain]
+(defn constrainScenario [_ scenario attributesLeftToConstrain]
   (let [attribute (rand-nth (seq attributesLeftToConstrain))
         editor (.getEditor (.getConstraintsFor scenario attribute))]
     (.removeValue editor
@@ -23,25 +23,30 @@
     (.applyTreatment scenario
       (TreatmentImpl/newSingletonTreatment attribute (.generateConstraints editor)))))
 
-(defn search [evaluationFunction initialScenario retries]
-  (let [states [(createStateFrom evaluationFunction initialScenario)]]
-    (letfn [(search-recur [scenario best-score attributesLeftToConstrain]
-              (when-not (empty? attributesLeftToConstrain)
-                (let [newScenario (constrainScenario scenario attributesLeftToConstrain)
+(defn search [evaluationFunction initialScenario constrain-scenario-fn retries]
+  (let [initialUnconstrainedAttributes (filter-for-unconstrained-attributes initialScenario)]
+    (letfn [(search-recur [scenario best-score attributesLeftToConstrain lives states]
+              (cond 
+                (= 0 lives) states
+                (empty? attributesLeftToConstrain)  (recur initialScenario best-score initialUnconstrainedAttributes (- lives 1) states)
+                true 
+                (let [newScenario (constrain-scenario-fn evaluationFunction scenario attributesLeftToConstrain)
                       newState (createStateFrom evaluationFunction newScenario)
                       newScore (.getScore newState)
                       better? (> newScore best-score)]
-                  (when (> newScore best-score)
-                    (conj states newState))
-                  (recur newScenario
+                  (recur
+                    newScenario
                     (if better? newScore best-score)
-                    (filter-for-unconstrained-attributes newScenario)))))]
-      (dotimes [i retries]
+                    (filter-for-unconstrained-attributes newScenario)
+                    lives
+                    (if better? (conj states newState) states)))))]
+      (let [initialState (createStateFrom evaluationFunction initialScenario)]
         (search-recur
           initialScenario
-          (.getScore (last states))
-          (filter-for-unconstrained-attributes initialScenario))))
-    states))
+          (.getScore initialState)
+          initialUnconstrainedAttributes
+          retries
+          [initialState])))))
 
 (defn se-impl-search [this evaluationFunction initialScenario]
-  (statesGenerator2path (search evaluationFunction initialScenario 25)));TODO make 25 a parameter
+  (statesGenerator2path (search evaluationFunction initialScenario constrainScenario 25)));TODO make 25 a parameter
